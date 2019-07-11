7111eb2272a4d6a5d63f8041db0a28d03aa621b9
amo95 <jameaamo@gmail.com>
2019-07-11 11:12:05 +0100

    new file update

---
+++ List/lists.py
@@ -0,0 +1,12 @@
+list = []
+list.append('a')
+print(list)
+
+
+def mimandmax(min,max):
+    ran = range(min,max)
+    for r in ran:
+        if r % 2 == 0:
+            print(r)
+    return r
+mimandmax(2,20)
